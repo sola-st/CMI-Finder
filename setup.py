390d74dc7845bab83c5ee6839e1ef347b100f8a3
islem-esi <fi_bouzenia@esi.dz>
2023-01-28 13:01:12 +0100

    minor change in setup files

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
-"""
-    Setup file for CMI-Finder-temp.
-    Use setup.cfg to configure your project.
 
-    This file was generated with PyScaffold 4.3.1.
-    PyScaffold helps you to put up the scaffold of your new Python project.
-    Learn more under: https://pyscaffold.org/
-"""
 from setuptools import setup
 
 if __name__ == "__main__":
